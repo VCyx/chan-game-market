0f5cdd86c285808801e1ca99122d3ba5aad5fcf7
Cyx <vcyx@ukr.net>
2023-03-15 12:15:26 +0200

    basic types

---
+++ src/models/card.ts
@@ -0,0 +1,17 @@
+import {Suit} from "@/models/suit";
+
+export type CardType = {
+    name: string;
+    image: string;
+    suit: Suit;
+    stats: CardStats;
+    skill: any;
+    character?: string[];
+}
+
+export type CardStats = {
+    move: number;
+    attack: number;
+    hp: number;
+    attackRange: number;
+}
---
+++ src/models/suit.ts
@@ -0,0 +1,6 @@
+export enum Suit {
+    HEARTS,
+    DIAMONDS,
+    SPADES,
+    CLUBS
+}
